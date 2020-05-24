f23e474f4a6010edd7025e71b5b4faa7e38f4b18
André Rodríguez Sabino <andresabinocalvo@gmail.com>
2020-05-24 18:08:42 +0100

    Simple Form HTML5+PHP

---
+++ envio.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Envio a Formularios</title>
+</head>
+<body>
+
+<?php
+    $valor = $_GET["v"];
+    echo "Valor digitado foi $valor";
+    ?>
+
+</body>
+</html>
